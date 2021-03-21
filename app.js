ae48e96af392344476aca18ff87bbc74c7aebab3
BasToTheMax <55401336+BasToTheMax@users.noreply.github.com>
2021-03-21 10:54:59 +0100

    v1.0.0

---
+++ app.js
@@ -0,0 +1,50 @@
+const { exec } = require('child_process');
+const chalk = require('chalk');
+
+info('--------------');
+info('By BasToTheMax');
+info('--------------');
+info('');
+info('');
+
+var count;
+count = 0;
+
+function start(file) {
+    count = count + 1;
+    exec('node '+file+'.js', (err, stdout, stderr) => {
+        if (err) {
+            // console.log(err);
+            count = count - 1;
+            error(`${chalk.red(`Stopped ${file}.js!`)} | ${chalk.green(`${count} bots online!`)}`);
+        }
+    }); 
+    info(`${chalk.green(`Started ${file}.js!`)}`);
+}
+function info(text) {
+    console.log(`${chalk.white('[')} ${chalk.green('-')} ${chalk.white(']')} ${text}`);
+}
+function error(text) {
+    console.log(`${chalk.white('[')} ${chalk.red('x')} ${chalk.white(']')} ${text}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+// MAIN PROGRAM! :D
+
+start('bot1');
+start('bot2');
+
+info('');
+info('');
+info('--------------')
+info(`${chalk.green(`Started ${count} bots!`)}`);
+info('--------------');
